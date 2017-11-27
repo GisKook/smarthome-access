918c0cdc194356578d30e913bb5c1652d7a5267a
zhangkai <zhangkai.gis@163.com>
2017-11-27 12:59:00 +0800

    add mutex to lock“

--- conns.go
+++ conns.go
@@ -1,6 +1,7 @@
 package sha
 
 import (
+	"sync"
 	"sync/atomic"
 )
 
@@ -8,6 +9,7 @@ type Conns struct {
 	connsindex map[uint32]*Conn
 	connsuid   map[uint64]*Conn
 	index      uint32
+	mutex      *sync.RWMutex
 }
 
 var connsInstance *Conns
@@ -18,6 +20,7 @@ func NewConns() *Conns {
 			connsindex: make(map[uint32]*Conn),
 			connsuid:   make(map[uint64]*Conn),
 			index:      0,
+			mutex:      new(sync.RWMutex),
 		}
 	}
 
@@ -25,19 +28,27 @@ func NewConns() *Conns {
 }
 
 func (cs *Conns) Add(conn *Conn) {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	conn.index = atomic.AddUint32(&cs.index, 1)
 	cs.connsindex[conn.index] = conn
 }
 
 func (cs *Conns) SetID(gatewayid uint64, conn *Conn) {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	cs.connsuid[gatewayid] = conn
 }
 
 func (cs *Conns) GetConn(uid uint64) *Conn {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	return cs.connsuid[uid]
 }
 
 func (cs *Conns) Remove(c *Conn) {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	delete(cs.connsindex, c.index)
 
 	connuid, ok := cs.connsuid[c.ID]
@@ -47,6 +58,8 @@ func (cs *Conns) Remove(c *Conn) {
 }
 
 func (cs *Conns) Check(uid uint64) bool {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	conn, ok := cs.connsuid[uid]
 	if ok {
 		_, realok := cs.connsindex[conn.index]
@@ -57,5 +70,7 @@ func (cs *Conns) Check(uid uint64) bool {
 }
 
 func (cs *Conns) GetCount() int {
+	cs.mutex.Lock()
+	defer cs.mutex.Unlock()
 	return len(cs.connsindex)
 }
